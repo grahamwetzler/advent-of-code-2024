09244674bb6df3fcef8320b8aa8ecb9cc0254107
Graham Wetzler <graham@wetzler.dev>
2024-12-03 09:57:44 -0600

    add tests

--- 03/03.py
+++ 03/03.py
@@ -36,5 +36,8 @@ def part_2(input):
     return total
 
 
+assert part_1("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))") == 161  # fmt: off
+assert part_2("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))") == 48  # fmt: off
+
 print("part 1:", part_1(input))
 print("part 2:", part_2(input))
